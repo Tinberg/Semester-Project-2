f17551ee7c1b5196631ebd359f7441dbcb062a63
mathias <tinberg92@hotmail.com>
2024-04-25 10:41:12 +0200

    config vite to include html pages for deployment

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        explore: 'html/explore.html',
+        listing: 'html/listing.html',
+        profile: 'html/profile.html',
+        myProfile: 'html/my-profile.html',
+        login: 'html/login.html',
+        register: 'html/register.html'
+      }
+    }
+  }
+});
